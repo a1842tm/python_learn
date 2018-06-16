9e89ad13bfee09afeed34fd3a488fe550dff95c7
a1842tm <40025558+a1842tm@users.noreply.github.com>
2018-06-16 06:17:18 +0000

    Create fizz_buzz.py

---
+++ fizz_buzz.py
@@ -0,0 +1,10 @@
+i = 1
+while i <= 20 :
+    if i % 3 == 0 :
+        print("fizz",end="")
+    if i % 5 == 0 :
+        print("buzz",end="")
+    if i % 3 != 0 and i % 5 != 0 :
+        print(i,end="")
+    print("")
+    i += 1
